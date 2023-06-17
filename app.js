31cdb1fad2d980bc5127221455581ae810ba04d1
hsynsnkul <hsynsnkul@gmail.com>
2023-06-17 19:25:41 +0300

    v2

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const addNumbersSpreadOperator=(...numbers)=>{
+    const sum=numbers.reduce((total,number)=>{
+    total+=number;
+    return total;
+    },0)
+
+    return sum;
+}
+
+console.log(addNumbersSpreadOperator(1,2,3,4,5,6,7));
