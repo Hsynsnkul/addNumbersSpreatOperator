cc7d99289c4955ad016210f548f4c0c8acba5671
huseyinsenkul <hsynsnkul@gmail.com>
2023-06-18 09:06:12 +0300

    operation file added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const addition=require('./addition');
-const multiplication=require('./multiplication')
+const addition=require('./operations/addition');
+const multiplication=require('./operations/multiplication')
 
 console.log(addition(1,2,3,4,5,6,7));
 console.log(multiplication(1,2,3,4,5,6,7));
