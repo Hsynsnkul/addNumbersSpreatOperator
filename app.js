bce6c6f76b6882b285ddfc0b3a2c724918ad995d
huseyinsenkul <hsynsnkul@gmail.com>
2023-06-18 08:15:51 +0300

    new numbers

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ const addNumbersSpreadOperator=(...numbers)=>{
     return sum;
 }
 
-console.log(addNumbersSpreadOperator(1,2,3,4,5,6,7));
+console.log(addNumbersSpreadOperator(1,2,3,4,5,6,7,8,9));
+
